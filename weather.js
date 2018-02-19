646a00cad2c44b9a0f21182db046c21a85dcd1e0
hsqza <30808819+hsqza@users.noreply.github.com>
2018-02-19 20:32:00 +0100

    Add files via upload
    Weather App init

---
+++ weather.js
@@ -0,0 +1,21 @@
+class Weather {
+
+    constructor(city, state) {
+        this.apiKey = 'ea388a8717867566';
+        this.city = city;
+        this.state = state;
+    }
+
+    async getWeather() {
+        const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+
+        const responseData = await response.json();
+
+        return responseData.current_observation;
+    }
+
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
